9108b2d59ceb4603c8ab10aed27ba25c9e439d58
Jupp Müller <jupp0r@gmail.com>
2016-03-31 18:03:21 +0200

    Refactor visitor: extract method

--- visitor.go
+++ visitor.go
@@ -22,42 +22,55 @@ func visitAST(cursor clang.Cursor, parent clang.Cursor, model *Model) clang.Chil
 	case clang.Cursor_Namespace:
 		return clang.ChildVisit_Recurse
 	case clang.Cursor_ClassDecl, clang.Cursor_StructDecl:
-		err := addClassToModel(model, cursor)
-		if err != nil {
-			return clang.ChildVisit_Continue
-		}
-		return clang.ChildVisit_Recurse
+		return visitClassDecl(model, cursor)
 	case clang.Cursor_CXXMethod:
-		if !cursor.CXXMethod_IsPureVirtual() {
-			return clang.ChildVisit_Continue
-		}
-
-		if cursor.Kind() == clang.Cursor_Destructor {
-			return clang.ChildVisit_Continue
-		}
-
-		classCursor := cursor.LexicalParent()
-		if classCursor.Kind() != clang.Cursor_ClassDecl && classCursor.Kind() != clang.Cursor_StructDecl {
-			return clang.ChildVisit_Continue
-		}
-
-		err := addMethodToModel(model, cursor)
-		if err != nil {
-			return clang.ChildVisit_Continue
-		}
-		return clang.ChildVisit_Recurse
+		return visitMethod(model, cursor)
 	case clang.Cursor_ParmDecl:
-		argument := argument{Name: cursor.Spelling(), Type: cursor.Type().Spelling()}
+		return visitParam(model, cursor)
+	}
+	return clang.ChildVisit_Continue
+}
 
-		parentMethod := parent.Spelling()
-		parentClass := parent.LexicalParent().Spelling()
-
-		method := model.Interfaces[parentClass].Methods[parentMethod]
-		methodArguments := append(model.Interfaces[parentClass].Methods[parentMethod].Arguments, argument)
-		method.Arguments = methodArguments
-		model.Interfaces[parentClass].Methods[parentMethod] = method
-
+func visitClassDecl(model *Model, cursor clang.Cursor) clang.ChildVisitResult {
+	err := addClassToModel(model, cursor)
+	if err != nil {
 		return clang.ChildVisit_Continue
 	}
+	return clang.ChildVisit_Recurse
+}
+
+func visitMethod(model *Model, cursor clang.Cursor) clang.ChildVisitResult {
+	if !cursor.CXXMethod_IsPureVirtual() {
+		return clang.ChildVisit_Continue
+	}
+
+	if cursor.Kind() == clang.Cursor_Destructor {
+		return clang.ChildVisit_Continue
+	}
+
+	classCursor := cursor.LexicalParent()
+	if classCursor.Kind() != clang.Cursor_ClassDecl && classCursor.Kind() != clang.Cursor_StructDecl {
+		return clang.ChildVisit_Continue
+	}
+
+	err := addMethodToModel(model, cursor)
+	if err != nil {
+		return clang.ChildVisit_Continue
+	}
+	return clang.ChildVisit_Recurse
+}
+
+func visitParam(model *Model, cursor clang.Cursor) clang.ChildVisitResult {
+	argument := argument{Name: cursor.Spelling(), Type: cursor.Type().Spelling()}
+
+	parent := cursor.LexicalParent()
+	parentMethod := parent.Spelling()
+	parentClass := parent.LexicalParent().Spelling()
+
+	method := model.Interfaces[parentClass].Methods[parentMethod]
+	methodArguments := append(model.Interfaces[parentClass].Methods[parentMethod].Arguments, argument)
+	method.Arguments = methodArguments
+	model.Interfaces[parentClass].Methods[parentMethod] = method
+
 	return clang.ChildVisit_Continue
 }
